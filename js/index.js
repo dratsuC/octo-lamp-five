c61d5656fa894bd0732bf5cd84dd5e0974f9e082
kaiden <dratsucc@protonmail.com>
2022-02-04 15:15:32 +1100

    Pankaj is the best

--- js/index.js
+++ js/index.js
@@ -20,10 +20,14 @@
 *************************** */
 
 window.addEventListener('DOMContentLoaded', () => {
+  let submitBool = false;
+  const submitButton = document.querySelector('#btnSubmit')
+  const resetButton = document.querySelector('#btnReset')  
   const start = document.querySelector('#start');
   start.addEventListener('click', function (e) {
     document.querySelector('#quizBlock').style.display = 'block';
     start.style.display = 'none';
+    if(!submitBool) setTime();
   });
   // quizArray QUESTIONS & ANSWERS
   // q = QUESTION, o = OPTIONS, a = CORRECT ANSWER
@@ -44,15 +48,25 @@ window.addEventListener('DOMContentLoaded', () => {
       o: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
       a: 1,
     },
-  ];
+    {
+      q: 'How many burger patties are in a big mac?',
+      o: ['One', 'Two', 'Three', 'Four'],
+      a: 1,
+    },  
+    {
+      q: 'Who is the best instructor ever?',
+      o: ['Albert Einstein', 'Confucius', 'Aristotle', 'Pankaj Alwani'],
+      a: 3,
+    },      
 
+  ]; 
   // function to Display the quiz questions and answers from the object
   const displayQuiz = () => {
     const quizWrap = document.querySelector('#quizWrap');
     let quizDisplay = '';
     quizArray.map((quizItem, index) => {
       quizDisplay += `<ul class="list-group">
-                   Q - ${quizItem.q}
+                   Q - <h3 class='text-light'>${quizItem.q}</h3>
                     <li class="list-group-item mt-2" id="li_${index}_0"><input type="radio" name="radio${index}" id="radio_${index}_0"> ${quizItem.o[0]}</li>
                     <li class="list-group-item" id="li_${index}_1"><input type="radio" name="radio${index}" id="radio_${index}_1"> ${quizItem.o[1]}</li>
                     <li class="list-group-item"  id="li_${index}_2"><input type="radio" name="radio${index}" id="radio_${index}_2"> ${quizItem.o[2]}</li>
@@ -62,29 +76,133 @@ window.addEventListener('DOMContentLoaded', () => {
       quizWrap.innerHTML = quizDisplay;
     });
   };
-
+  // console.log(quizArray)
   // Calculate the score
+
+  //defining counters globally (sorry)
+  // to share them between functions
+  let answerCounter = 0;
+  let score = 0;
   const calculateScore = () => {
-    let score = 0;
+    
+    
     quizArray.map((quizItem, index) => {
       for (let i = 0; i < 4; i++) {
         //highlight the li if it is the correct answer
         let li = `li_${index}_${i}`;
         let r = `radio_${index}_${i}`;
+        // console.log(quizItem, index)
         liElement = document.querySelector('#' + li);
         radioElement = document.querySelector('#' + r);
-
-        if (quizItem.a == i) {
-          //change background color of li element here
-        }
-
+        // console.log(radioElement)
+        // defining necessary elements (
+        // id of current radio, and 
+        // id of its answer)
+        let radioId = radioElement.id;
+        let lastChar = radioId.substr(radioId.length - 1);        
+        // start of task 1
         if (radioElement.checked) {
-          // code for task 1 goes here
-        }
+          // documenting presence of answer
+          answerCounter++;
+
+          // checking if current answer is correct
+          if(lastChar == quizItem.a) {
+            console.log('correct answer');
+            score++
+            radioElement.parentElement.classList.add('bg-success');
+            console.log(radioElement)
+          } else {
+            radioElement.parentElement.classList.add('bg-danger');
+            console.log(quizArray[index].a)
+          }
+          
+        } else if (submitBool && (lastChar == quizItem.a)) {
+            radioElement.parentElement.classList.add('bg-success');
+          }
+      } //end of loop
+    }); //end of .map()
+  }; // end of calculateScore
+  // if answer counter equals 
+  // total number of questions, 
+  // display score
+  const showScore = () => {
+    const scoreSpan = document.querySelector('#score');
+    if (answerCounter == quizArray.length) {
+      console.log(`score: ${score}`)
+      if (score < 3) {scoreSpan.innerHTML = 
+        `Bad luck bud, your score was <strong>${score}</strong>.`}
+      else {scoreSpan.innerHTML = 
+        `Great work champ, your score was <strong>${score}</strong>`}
+
+      // once sufficient answers are given, function finishes
+      // submitButton.classList.add('disabled = "disabled"')
+
+      return true;
+    }      
+  }  
+
+
+  // controls the timer
+  const setTime = () => {
+
+    // Set the date we're counting down to
+    var timerLimit = new Date().getTime() + 120000;
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = timerLimit - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Display the result in the element with id="demo"
+      document.getElementById("time").innerHTML = minutes + "m " + seconds + "s ";
+      console.log(submitBool);
+      // If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("time").innerHTML = "Time's Up Pal!";
+      } else if (submitBool === true) {
+        clearInterval(x);
+        document.getElementById("time").innerHTML = "Nice one bud, just in time!";        
       }
-    });
-  };
+    }, 1000);  
+
+  } //end of checkTime
+
+  // function reset()  {
+  //   document.getElementById("time").innerHTML = "Just in time, guy!";
+  //   // remove 'disabled' class from submit button
+  // }
+    
+
 
   // call the displayQuiz function
   displayQuiz();
-});
+
+
+
+  resetButton.addEventListener('click', () => {
+    window.location.reload();
+  })
+
+  // submit button event listener
+  submitButton.addEventListener('click', () => {
+  // if submit button is clicked, make sure all
+  // radios were picked
+    submitBool = true;
+    calculateScore();
+    showScore();
+    answerCounter = 0;
+    // reset()
+  })
+
+
+
+}); //end of DOM listener
